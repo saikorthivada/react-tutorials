e41d18038a4bbfc6caf2d70fa3cd672366fa53c1
Sai kumar skk <korthivadasaikumar@gmail.com>
2024-09-22 17:31:54 -0400

    Lazy routes implementation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
 import Navbar from './components/navbar/Navbar';
-import Products from './pages/products/Products';
-import Details from './pages/products/details/Details';
+import { lazy, Suspense } from 'react';
+
+const Login = lazy(() => import('./pages/login/Login'));
+const Register = lazy(() => import('./pages/register/Register'));
+const Products = lazy(() => import('./pages/products/Products'));
+const Details = lazy(() => import('./pages/products/details/Details'));
 
 function App() {
   return (
     <div className='App'>
-
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='' element={<Login />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
+      <Suspense fallback={<h1>Loading</h1>}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='' element={<Login />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
           <Route path='products' element={<Products />}>
-            <Route index element={<Details />} />
-            <Route path=':id' element={<Details />}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+              <Route index element={<Details />} />
+              <Route path=':id' element={<Details />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }

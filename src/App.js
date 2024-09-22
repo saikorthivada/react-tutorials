4d3c3c63eea476b5edfd1fcf318cafb692059d40
Sai kumar skk <korthivadasaikumar@gmail.com>
2024-09-22 18:08:43 -0400

    Protected routes

--- src/App.js
+++ src/App.js
@@ -2,31 +2,45 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar/Navbar';
-import { lazy, Suspense } from 'react';
+import { createContext, lazy, Suspense, useState } from 'react';
 
 const Login = lazy(() => import('./pages/login/Login'));
 const Register = lazy(() => import('./pages/register/Register'));
 const Products = lazy(() => import('./pages/products/Products'));
 const Details = lazy(() => import('./pages/products/details/Details'));
 
+export const AuthContext = createContext();
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
   return (
-    <div className='App'>
-      <Suspense fallback={<h1>Loading</h1>}>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path='' element={<Login />} />
-            <Route path='login' element={<Login />} />
-            <Route path='register' element={<Register />} />
-          <Route path='products' element={<Products />}>
-              <Route index element={<Details />} />
-              <Route path=':id' element={<Details />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Suspense>
-    </div>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      <div className='App'>
+        <Suspense fallback={<h1>Loading</h1>}>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path='' element={<Login />} />
+              <Route path='login' element={<Login />} />
+              <Route path='register' element={<Register />} />
+              <Route path='products' element={<ProtectedRoute>
+                <Products />
+              </ProtectedRoute>}>
+                <Route index element={<ProtectedRoute>
+                  <Details />
+                </ProtectedRoute>} />
+                <Route path=':id' element={<ProtectedRoute>
+                  <Details />
+                </ProtectedRoute>} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../App';
 
 function Login() {
+
+    const {login} = useContext(AuthContext);
 
     const navigate = useNavigate();
     const navigateToRegister = () => {
         console.log('clicked');
         navigate('/register');
     }
+
+    const loginAndNavigate = () => {
+        login();
+        navigate('/products');
+    }
     return (
         <div>
             <button className='btn btn-primary' onClick={() => navigateToRegister()}>Navigate to register</button>
+            &nbsp;
+            <button className='btn btn-primary' onClick={() => loginAndNavigate()}>Login and navigate to products</button>
         </div>
     )
 }

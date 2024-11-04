ae94144ebe84c10c690bc129bf6e93787a2f62dc
Sai kumar <korthivadasaikumar@gmail.com>
2024-11-04 00:10:55 -0500

    react router data API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
-import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className='App'>
-
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='' element={<Login />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>Component API vs Data API's</h1>
     </div>
   );
 }

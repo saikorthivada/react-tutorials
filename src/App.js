ca2f1a0668b6deff01e14a2856bd10cbfbc1b904
Sai kumar <korthivadasaikumar@gmail.com>
2024-11-04 17:41:48 -0500

    Data API basic navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, Link, NavLink, RouterProvider } from 'react-router-dom';
+import Login from './Login/Login';
+import Register from './Register/Register';
 
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <Login />
+  },
+  {
+    path: 'login',
+    element: <Login />
+  },
+  {
+    path: 'register',
+    element: <Register />
+  }
+]);
 function App() {
   return (
     <div className='App'>
       <h1>Component API vs Data API's</h1>
+      <RouterProvider router={router}/>
     </div>
   );
 }

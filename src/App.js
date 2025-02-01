f17e52b0387dacf6000aec885a51791bd2022d8b
Sai Kumar korthivada <korthivadasaikumar@gmail.com>
2025-02-01 16:23:06 -0500

    Item list manager in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleItemList = () => {
+    if (input) {
+      setList([...list, input]);
+      setInput("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='d-flex justify-content-center'>
+        <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Item List Manager</h5>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">Item Name</label>
+              <input 
+                type="text"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Enter Item Name"
+                value={input}
+                onChange={(event) => setInput(event.target.value)}/>
+            </div>
+            <button className='btn btn-primary' onClick={handleItemList}>Add Item</button>
+
+            <div>
+              <ul>
+                {
+                  list.map((item) => <li>{item}</li>)
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
